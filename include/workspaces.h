b7afac3ba9cf806f58e68036fcccd5dea77b8721
dmitryemacs <dimadimof81@gmail.com>
2025-10-30 19:52:09 +0000

    update switch and move workspaces functions

--- include/workspaces.h
+++ include/workspaces.h
@@ -9,7 +9,7 @@ extern int current_workspace;
 extern Window workspace_windows[WORKSPACE_COUNT][100];
 extern int window_count[WORKSPACE_COUNT];
 
-void switch_to_workspace(int workspace_index);
-void move_window_to_workspace(Window window, int workspace_index);
+void switch_to_workspace(Display *display, int workspace_index);
+void move_window_to_workspace(Display *display, Window window, int workspace_index);
 
 #endif
--- src/workspaces.c
+++ src/workspaces.c
@@ -3,21 +3,29 @@
 #include <stdio.h>
 
 int current_workspace = 0;
-Window workspace_windows[WORKSPACE_COUNT][100];
+Window workspace_windows[WORKSPACE_COUNT][100] = {0};
 int window_count[WORKSPACE_COUNT] = {0};
 
-void switch_to_workspace(int workspace_index) {
+void switch_to_workspace(Display *display, int workspace_index) {
     if (workspace_index >= 0 && workspace_index < WORKSPACE_COUNT && workspace_index != current_workspace) {
+        for (int i = 0; i < window_count[current_workspace]; i++) {
+            XUnmapWindow(display, workspace_windows[current_workspace][i]);
+        }
+
+        for (int i = 0; i < window_count[workspace_index]; i++) {
+            XMapWindow(display, workspace_windows[workspace_index][i]);
+        }
+
         current_workspace = workspace_index;
         printf("Switched to workspace %d\n", workspace_index);
     }
 }
 
-void move_window_to_workspace(Window window, int workspace_index) {
+void move_window_to_workspace(Display *display, Window window, int workspace_index) {
     if (workspace_index >= 0 && workspace_index < WORKSPACE_COUNT) {
         for (int i = 0; i < window_count[current_workspace]; i++) {
             if (workspace_windows[current_workspace][i] == window) {
-                for (int j = i; j < window_count[current_workspace] - 1; j++) {
+                for (int j = i; j < window_count[current_workspace] + 1; j++) {
                     workspace_windows[current_workspace][j] = workspace_windows[current_workspace][j + 1];
                 }
                 window_count[current_workspace]--;
@@ -27,6 +35,12 @@ void move_window_to_workspace(Window window, int workspace_index) {
 
         workspace_windows[workspace_index][window_count[workspace_index]] = window;
         window_count[workspace_index]++;
+
+        if (workspace_index != current_workspace) {
+            XUnmapWindow(display, window);
+        } else {
+            XMapWindow(display, window);
+        }
 
         printf("Moved window to workspace %d\n", workspace_index);
     }
